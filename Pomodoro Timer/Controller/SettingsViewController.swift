a60095c666cb2ba84e4ca1e8bad7b8e378c87206
Aidos Mukatayev <mukatayev19@gmail.com>
2020-10-08 00:37:43 +0900

    Installed CocoaPods and made minor changes in other files

--- Pomodoro Timer/Controller/SettingsViewController.swift
+++ Pomodoro Timer/Controller/SettingsViewController.swift
@@ -1,67 +1,3 @@
-////
-////  SettingsViewController.swift
-////  Pomodoro Focus Timer
-////
-////  Created by AZM on 2020/10/03.
-////
-//
-//import UIKit
-//
-//private let reuseIdentifier = "SettingsCell"
-//
-//class SettingsViewController: UIViewController {
-//
-//    var tableView: UITableView!
-//
-//
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        view.backgroundColor = .white
-//        overrideUserInterfaceStyle = .light
-//        navigationController?.navigationBar.prefersLargeTitles = true
-//        navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
-//
-//        configureUI()
-//    }
-//
-//    func configureTableView() {
-//        tableView = UITableView()
-//        tableView.delegate = self
-//        tableView.dataSource = self
-//        tableView.rowHeight = 60
-//
-//        tableView.register(SettingsCell.self, forCellReuseIdentifier: reuseIdentifier)
-//        view.addSubview(tableView)
-//        tableView.frame = view.frame
-//    }
-//
-//    func configureUI() {
-//        configureTableView()
-//
-//        //        navigationController?.navigationBar.isTranslucent = false
-//        //        navigationController?.navigationBar.barStyle = .black
-//    }
-//}
-//
-//extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
-//
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        return 2
-//    }
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 5
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! SettingsCell
-//        return cell
-//    }
-//
-//
-//}
-
 //
 //  SettingsViewController.swift
 //  Pomodoro Focus Timer
@@ -135,14 +71,15 @@ class SettingsViewController: UITableViewController {
         mySwitch.onTintColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
         mySwitch.addTarget(self, action: #selector(switchSwitched), for: .valueChanged)
         
-//        cell.accessoryView = mySwitch
+        
+        
         
         switch indexPath.section {
         case 0: cell.backgroundColor = .white
         case 1: cell.accessoryView = mySwitch
         default: break
         }
-        
+        cell.selectionStyle = .none
         return cell
     }
     
--- Pomodoro Timer/View/CustomPickerView.swift
+++ Pomodoro Timer/View/CustomPickerView.swift
@@ -1,1 +1,48 @@
+//
+//  TimerControlToggleButton.swift
+//  Pomodoro Focus Timer
+//
+//  Created by AZM on 2020/10/03.
+//
 
+import UIKit
+
+class CustomPickerView: UIPickerView, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    //MARK: - Properties
+
+    var minutes = [1...60]
+    var seconds = [1...60]
+
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0: return 59
+        case 1: return 59
+        default: return 0
+        }
+    }
+
+
+    //MARK: - initializer
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initPickerView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func initPickerView() {
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        pickerView.dataSource = self
+    }
+
+}
