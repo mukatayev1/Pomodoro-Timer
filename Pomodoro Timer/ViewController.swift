139af22b6ebdb6d5f55f56106dd3c1f8d8ea071b
Aidos Mukatayev <mukatayev19@gmail.com>
2020-10-07 13:10:25 +0900

    Initial Commit

---
+++ Pomodoro Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pomodoro Timer
+//
+//  Created by AZM on 2020/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
